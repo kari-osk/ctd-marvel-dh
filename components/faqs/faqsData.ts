1ca3ae392c73375f622c801f8981e345751b5b05
kari-osk <karinaosuka@gmail.com>
2023-04-08 22:34:02 -0300

    Add faq page

--- components/faqs/faqsData.ts
+++ components/faqs/faqsData.ts
@@ -1,38 +1,38 @@
 export type FaqsType = {
-  id: number;
+  id: string;
   question: string;
   answer: string;
 };
 
 export const faqsData: FaqsType[] = [
   {
-    id: 1,
+    id: "panel1",
     question: "Quantos comics eles têm?",
     answer:
       "Atualmente temos toda a coleção Marvel. Algumas cópias podem ter pouca ou nenhuma disponibilidade no momento. Para mais informações você pode acessar https://marvel.com",
   },
   {
-    id: 2,
+    id: "panel2",
     question: "Podemos reservar novos lançamentos?",
     answer:
       "Infelizmente nosso site ainda não aceita reservas antecipadas. Mas estamos trabalhando nessa funcionalidade. Siga nosso twitter para ficar por dentro das novidades.",
   },
   {
-    id: 3,
-    question: "Quanto tempo demoram as entregas?",
+    id: "panel3",
+    question: "Qual o prazo de entrega?",
     answer:
       "Todas as nossas entregas são enviadas através da DH-Express, que chega a todo o país em 24 horas.",
   },
   {
-    id: 4,
-    question: "Que métodos de pagamento estão disponíveis?",
+    id: "panel4",
+    question: "Formas de pagamento disponíveis?",
     answer:
       "Apenas cartões de crédito Visa e Mastercard são aceitos. No momento não aceitamos pagamentos em dinheiro ou outros meios.",
   },
   {
-    id: 5,
-    question: "Aceitamos devoluções?",
+    id: "panel5",
+    question: "Política de devolução?",
     answer:
-      "Nossas compras aceitam devoluções desde que o quadrinho esteja em sua embalagem original, caso contrário perdem o valor de revenda. Caso deseje devolvê-lo e ele estiver nas mesmas condições em que foi enviado, entre em contato com 11-5555-0001 para resolver a devolução.",
+      "Nossas compras aceitam devoluções desde que o quadrinho esteja em sua embalagem original, caso contrário perdem o valor de revenda. Caso deseje devolvê-lo e ele estiver nas mesmas condições em que foi enviado, entre em contato com 11-5555-0001 para realizar a devolução.",
   },
 ];
---
+++ pages/faq.page.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import { FaqsType, faqsData } from "dh-marvel/components/faqs/faqsData";
+import { Button, Container, Grid } from "@mui/material";
+import { useRouter } from "next/router";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState<string | false>("");
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
+
+  const { back } = useRouter();
+
+  return (
+    <Container
+      style={{
+        minHeight: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        sx={{ my: 2 }}
+        variant="outlined"
+        size="small"
+        startIcon={<NavigateBeforeIcon />}
+        onClick={back}
+      >
+        Voltar para a página anterior
+      </Button>
+      <Grid container>
+        <Grid item direction="column" >
+          <Typography variant="h1" sx={{textAlign: "center", paddingTop: "2rem", paddingBottom: "4rem"}}>Perguntas frequentes</Typography>
+          {faqsData.map((faq: FaqsType) => {
+            const { id, question, answer } = faq;
+            return (
+              <>
+                <Accordion
+                  expanded={expanded === id}
+                  onChange={handleChange(id)}
+                >
+                  <AccordionSummary
+                    aria-controls="panel1d-content"
+                    id="panel1d-header"
+                  >
+                    <Typography>{question}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>{answer}</Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
